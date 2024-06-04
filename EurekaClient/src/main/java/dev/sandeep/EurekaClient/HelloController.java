cf884a21f7c5d0c2b899c8d66f0429bdf60c9b89
sndpoffcl <sandeep2official@gmail.com>
2024-06-04 22:55:54 +0530

    Eureka Demo

---
+++ EurekaClient/src/main/java/dev/sandeep/EurekaClient/HelloController.java
@@ -0,0 +1,13 @@
+package dev.sandeep.EurekaClient;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public interface HelloController {
+
+    @RequestMapping("/greeting")
+    String greeting();
+}
